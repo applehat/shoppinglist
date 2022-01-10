b9803940da54df476b025044b4c5d6007f532f50
Cameron Chunn <cameronchunn@gmail.com>
2022-01-09 19:11:38 -0600

    Loading data, partial display logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Toolbar, Typography, CircularProgress, Container, Stack, Box, Checkbox, Grid } from '@mui/material'
+import ModeEditOutlinedIcon from '@mui/icons-material/ModeEditOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
+/**
+ * Main App component
+ * @return {JSX.Element}
+ */
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([])
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () =>{
+      // groq query for fetching data
+      const query = `
+      *[_type=="item"]{
+        _id,
+        name,
+        description,
+        complete
+      }`
+      setLoading(true)
+      try {
+        const {data: response} = await axios.get(`http://localhost:3008/documents?query=${encodeURIComponent(query)}`)
+        setData(response?.result || [])
+        setError(response?.error || false)
+      } catch (error) {
+        setError(error.message)
+        console.error(error.message)
+      }
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar variant="dense" >
+          <Typography variant="h6" color="inherit" component="div">
+            Shopping List
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container fixed>
+      {loading && (<CircularProgress />)}
+      {error && (<div>There was an error: {error}</div>)}
+      {!loading && !error && (
+        <Stack spacing={2}>
+          {data.map(item => {
+            console.log(item)
+            let boxStyle = {border: '0.5px solid #D5DFE9', borderRadius: '4px'}
+            if (item.complete) {
+              boxStyle = {background: 'rgba(213, 223, 233, 0.17)', textDecoration: 'line-through', ...boxStyle}
+            }
+            return (
+              <Box component="div" key={item._id} sx={boxStyle}>
+                <Grid container spacing={1} alignItems="center">
+                  <Grid item xs={1} sx={{textAlign: 'center'}}>
+                    <Checkbox
+                      checked={item.complete}
+                      onChange={() => { console.log(checked) }}
+                      sx={{ '& .MuiSvgIcon-root': { fontSize: 30 } }}
+                      />
+                  </Grid>
+                  <Grid item xs={10}>
+                    <Box component="div" sx={{margin: '24px 0px'}}>
+                      <Stack spacing={1}>
+                        <Box component="div" sx={{fontWeight: 600, fontSize: '16px'}}>
+                          {item.name}
+                        </Box>
+                        <Box component="div" sx={{fontWeight: 400, fontSize: '14px'}}>
+                          {item.description}
+                        </Box>
+                      </Stack>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <Stack spacing={2} direction="row">
+                      <ModeEditOutlinedIcon/>
+                      <DeleteOutlineOutlinedIcon/>
+                    </Stack>
+                  </Grid>
+                </Grid>
+              </Box>
+            )
+          })}
+        </Stack>
+      )}
+      
+      </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import '@fontsource/dosis'
+import '@fontsource/nunito'
+
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#4D81B7',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  typography: {
+    fontFamily: '"Nunito", sans-serif',
+    h6: {
+      fontFamily: '"Dosis", sans-serif',
+      textTransform: 'uppercase',
+      padding: '1rem',
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Nunito', sans-serif;
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          line-height: 22px;
+          font-size: 16px;
+        }
+      `,
+    },
+  },
+});
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
